e143702a7b5aaf0a9e25b731e0c6adb1dcd39250
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2019-04-17 02:09:14 +0700

    Tools versi: 1.2

---
+++ gratis-sms.py
@@ -0,0 +1,32 @@
+#sms_gratis_2019
+
+#!/usr/bin/python
+
+# Author : Rusmana
+# Cita-Cinta : ingin menjadi developer and programer Aamiin
+
+
+import sys
+import time
+import os
+from platform import system
+os.system("pkg install java")
+os.system("pkg install bash")
+os.system("bash i.sh")
+os.system("pkg install python2")
+os.system("git clone https://github.com/amsitlab/smsid-java")
+os.system("cd smsid-java")
+os.system("ls")
+os.system("sh xxkxjakaqkqpqpKKzlLajalaqpqkskxLalajznaoqkamZnLalMMLzlkznaa.sh")
+print
+print
+
+def q():
+    time.sleep(1)
+    os.system("smsid send " + z)
+    os.system("sh ulansnxnwoqpqkashzhzkakamabsakakaoaoaoajsnzmMLalzmamammsnzmalaxnzka.sh")
+print
+	
+z = raw_input("\033[37;1m[\033[31;1m+\033[37;1m]\033[32;1mMASUKAN NOMOR NYA \033[31;1m:\033[37;1m ")
+q()
+sys.exit()
---
+++ i.sh
@@ -0,0 +1,14 @@
+###################################
+# Author by: Mr.Tr3v!0n           #
+# Team : Black Coder Crush        #
+# Di buat : Selasa, 16 April 2019 #
+# Selesai Jam : 10:3              #
+###################################
+# Hanya seorang anak biasa yang gk bisa apa2😢 selalu tersakiti nolep 
+# gk pernah ada yg bisa mengerti dgn perasaan ku gmn baik itu org tua😢
+
+
+
+z="
+";kBz='g pa';EBz='to r';cz='slee';xBz='\033';XBz='l fa';LBz='by t';wz='1mTh';RCz='inst';UBz='ols"';IBz=' ins';ZBz='exit';ICz='e in';BCz='${PK';fz='ecki';gBz='eady';Wz='3[33';iz='retm';PBz='kg i';sz='M}" ';Yz=' "Pl';rz='VIKV';aBz='else';fBz=' alr';TBz='x-to';pz='x "$';lz='3[0m';DBz='kvm ';KBz=' it ';cBz='mdal';NCz='n yo';Dz='ich ';lBz='ckag';HCz='ll b';OBz='"\tp';Ez='dalv';nz='if [';bBz='3[36';OCz='u wa';Oz='b";';Xz='m";';bCz=' "${';Tz='echo';Fz='ikvm';qz='{DAL';dBz='vikv';Hz='PKG_';ACz='[33m';KCz=' "Ru';Pz='NAME';Bz='IKVM';VBz=' "In';Mz='1_al';tz=']; t';JBz='tall';pBz='_FIL';QCz='o un';jBz='ndin';JCz='l.";';ECz='0m"';dz='p 3';Zz='ease';GCz='e wi';bz='t.."';yBz='[0m ';QBz='nsta';mz='";';Uz=' -e ';uBz='find';Qz='="sm';az=' wai';kz='..";';hz='equi';WBz='stal';Cz='=`wh';RBz='ll t';SCz='all.';DCz='LE}\';yz='rogr';eBz='m\03';WCz='e ${';FCz=' "Pa';qBz='E}" ';Nz='l.de';Gz='`';vz='3[03';SBz='ermu';uz='hen';FBz='un.\';HBz='0m";';ABz='am u';Az='DALV';MCz=' whe';Sz=';';PCz='nt t';aCz=' apt';Iz='FILE';BBz='se d';Vz='"\03';TCz='"\ta';xz='is p';nBz='f "$';LCz='n it';GBz='033[';NBz='g:";';vBz=' pac';CCz='G_FI';UCz='pt r';iBz=' "Fi';wBz='kage';YCz='ling';ez=' "Ch';Lz='d_1.';Jz='="./';jz='ents';hBz='fi';ZCz='exec';MBz='ypin';rBz='3[31';oBz='{PKG';tBz='not ';YBz='iled';mBz='e.."';Rz='sid"';gz='ng r';sBz='mCan';oz=' ! -';XCz='}";';Kz='smsi';VCz='emov';CBz='alvi';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$z$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$z$Hz$Pz$Qz$Rz$Sz$z$Tz$Uz$Vz$Wz$Xz$z$Tz$Yz$Zz$az$bz$Sz$z$cz$dz$z$Tz$ez$fz$gz$hz$iz$jz$kz$z$cz$dz$z$Tz$Uz$Vz$lz$mz$z$nz$oz$pz$qz$rz$sz$tz$uz$z$Tz$Uz$Vz$vz$wz$xz$yz$ABz$BBz$CBz$DBz$EBz$FBz$GBz$HBz$z$Tz$Yz$Zz$IBz$JBz$KBz$LBz$MBz$NBz$z$Tz$Uz$OBz$PBz$QBz$RBz$SBz$TBz$UBz$Sz$z$Tz$VBz$WBz$XBz$YBz$mz$z$ZBz$Sz$z$aBz$z$Tz$Uz$Vz$bBz$cBz$dBz$eBz$lz$fBz$gBz$mz$z$hBz$z$Tz$iBz$jBz$kBz$lBz$mBz$z$cz$dz$z$nz$oz$nBz$oBz$pBz$qBz$tz$uz$z$Tz$Uz$Vz$rBz$sBz$tBz$uBz$vBz$wBz$xBz$yBz$xBz$ACz$BCz$CCz$DCz$GBz$ECz$z$Tz$VBz$WBz$XBz$YBz$mz$z$ZBz$Sz$z$hBz$z$Tz$FCz$lBz$GCz$HCz$ICz$WBz$JCz$z$Tz$KCz$LCz$MCz$NCz$OCz$PCz$QCz$RCz$SCz$mz$z$Tz$Uz$TCz$UCz$VCz$WCz$Hz$Pz$XCz$z$Tz$VBz$WBz$YCz$vBz$wBz$mz$z$cz$dz$z$ZCz$aCz$IBz$JBz$bCz$Hz$Iz$XCz" 
