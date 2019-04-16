f11d3c342d7622b4baf1336adb7a628320dadf48
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2019-04-16 22:20:40 +0700

    Script 1.2

---
+++ ulansnxnwoqpqkashzhzkakamabsakakaoaoaoajsnzmMLalzmamammsnzmalaxnzka.sh
@@ -0,0 +1,13 @@
+###################################
+# Author by: Mr.Tr3v!0n           #
+# Team : Black Coder Crush        #
+# Di buat : Selasa, 16 April 2019 #
+# Selesai Jam : 10:3              #
+###################################
+# Hanya seorang anak biasa yang gk bisa apa2😢 selalu tersakiti nolep 
+# gk pernah ada yg bisa mengerti dgn perasaan ku gmn baik itu org tua😢
+
+z="
+";Yz='grat';cz='elif';lz='en';Tz='hen';Wz='pyth';fz='im =';gz=' t ]';Ez='esan';Dz='im P';Rz=' = Y';Oz='] ||';Mz='rim ';ez='$kir';kz='];th';mz='exit';Bz=' -p ';Xz='on2 ';jz='= T ';Kz='if [';hz=' || ';Iz='" ki';Uz='clea';nz='fi';dz='  [ ';Qz='irim';Cz='"Kir';Gz='i y/';iz='[ ki';Nz='= y ';Zz='is-s';Sz=' ];t';Hz='t : ';Az='read';Pz=' [ k';Lz=' $ki';Vz='r';Fz=' lag';Jz='rim';az='ms.p';bz='y';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$z$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$z$Uz$Vz$z$Wz$Xz$Yz$Zz$az$bz$z$cz$dz$ez$fz$gz$hz$iz$Mz$jz$kz$lz$z$mz$z$nz" 
+z="
